26dd7b9f8977555999c4bf1a8f12a3356a240917
Shriram Mishra <ram.techcare27@gmail.com>
2022-04-14 00:19:04 +0530

    updation in serverjs

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/googlelogin'));
+app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/googlelogin/index.html')); });
+app.listen(process.env.PORT || 8080);
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
