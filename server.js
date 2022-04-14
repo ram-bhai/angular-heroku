533fc516372ace4496cc6f6ef910aabbcd05463c
Shriram Mishra <ram.techcare27@gmail.com>
2022-04-14 11:34:03 +0530

    serverjs updation

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/googlelogin'));
-app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/googlelogin/index.html')); });
+app.get('/signin', function(req, res) { res.sendFile(path.join(__dirname + '/dist/googlelogin/index.html')); });
 app.listen(process.env.PORT || 8080);
 
-app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server listening on the port::${port}`);
+// });
